7dbfa272214b0b260444f9494637d86073106e49
Vedang <vedang2607@gmail.com>
2025-02-21 12:28:47 +0530

    createVendor is done

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -5,9 +5,58 @@ const { v4: uuidv4 } = require("uuid");
 
 // creating a new vendor
 exports.createVendor = async (req, res) => {
-    try{
-        const { user_id, store_name, business_email, business_phone, store_description}
-    }catch(error){
-        //console.error("")
-    }
+
+    // invoking multer middleware
+    uploadDocument(req, res, async(err) => {
+        if(err){
+            console.error("Error in file uploading", error);
+            return res.status(400).json({
+                success: false,
+                error: "File upload failed!"
+            });
+        }
+        try{
+            const { user_id, store_name, business_email, business_phone, store_description, store_address} = req.body;
+
+            // generate vendor_id
+            const vendor_id = uuidv4();
+
+            // file upload
+            let documentUrl = null;
+            if(req.file) {
+                const uploadedDoc = await uploadToCloudinary(req.file.buffer, "vendors/documents");
+                documentUrl = uploadedDoc.secure_url;
+            }
+            if(documentUrl){
+                console.log("Document url is ", documentUrl);
+            }else{
+                console.error("document is not uploaded");
+            }
+
+            // saving vendor in database
+            const newVendor = await Vendor.create({
+                vendor_id,
+                user_id,
+                store_name,
+                business_email,
+                business_phone,
+                store_description,
+                store_address,
+                status: "pending",
+                date: new Date(),
+                document: documentUrl,
+                certificate: null
+            });
+
+            res.status(201).json({
+                success: true,
+                message: "Store is registered successfully!"
+            });
+        }catch(error){
+            console.error("Error in registering a store", error);
+            res.status(500).json({
+                error: "Registration of store failed"
+            });
+        }
+    })
 };
