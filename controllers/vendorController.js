6c7f37c62aa5d95b1397a94a4c37a53625fd38f5
Vedang <vedang2607@gmail.com>
2025-02-21 11:46:33 +0530

    streamifier pkg installed

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -3,12 +3,7 @@ const Vendor = require('../models/vendor');
 // creating a new vendor
 exports.createVendor = async (req, res) => {
     try{
-        //const vendor = await Vendor.create(req.body);
-        res.status(201).json({
-            success: true,
-            message: 'Vendor is registered successfully',
-            data: vendor
-        });
+        const { user_id, store_name, business_email, business_phone, store_description}
     }catch(error){
         //console.error("")
     }
