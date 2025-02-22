7062b5da54935407270ae18a901c21ef1bc2c2f0
Vedang <vedang2607@gmail.com>
2025-02-22 09:51:07 +0530

    CRUD operations are done

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -61,14 +61,106 @@ exports.createVendor = async (req, res) => {
     })
 };
 
-// update the store status
-exports.updateStoreStatus = async (req, res) => {
+// get all vendors
+exports.getAllVendors = async (req, res) => {
     try{
-        const { vendor_id, status } = req.body;
-
-        const validStatuses = ["pending", "approved", "rejected"];
-        if(!validStat)
+        const vendors = await Vendor.findAll();
+        return res.status(200).json({
+            success: true,
+            vendors
+        });
     }catch(error){
-        
+        console.log("Error in retriwving all Vendors: ", error.message);
+        return res.status(500).json({
+            success: false, 
+            message: error.message
+        });
     }
-}; 
+};
+
+// Get vendor by ID
+exports.getVendorById = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const vendor = await Vendor.findByPk(id);
+
+        if(!vendor){
+            return res.status(404).json({
+                success: false,
+                message: 'Vendor not found'
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            vendor
+        });
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+// Update a Vendor
+exports.updateVendor = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const updatedData = req.body;
+
+        const vendor = await Vendor.findByPk(id);
+        if(!vendor){
+            return res.status(404).json({
+                success: false,
+                message: 'Vendor not found'
+            });
+        }
+        await vendor.update(updatedData);
+        return res.status(200).json({
+            success: true,
+            message: 'Vendor updated successfully'
+        });
+    }catch(error){
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+// delete a Vendor
+exports.deleteVendor = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const vendor = await Vendor.findByPk(id);
+        if(!vendor){
+            return res.status(404).json({
+                success: false,
+                message: 'Vendor not found'
+            });
+        }
+        await vendor.destroy();
+        return res.status(200).json({
+            success: true,
+            message: 'Vendor deleted successfully'
+        });
+    }catch(error){
+        console.error('Error in deleting a vendor', error.message);
+        return res.status(500).json({
+            success: false,
+            message: 'Failed in deleting a Vendor'
+        });
+    }
+}
+// update the store status
+// exports.updateStoreStatus = async (req, res) => {
+//     try{
+//         const { vendor_id, status } = req.body;
+
+//         const validStatuses = ["pending", "approved", "rejected"];
+//         if(!validStat)
+//     }catch(error){
+        
+//     }
+// }; 
