6ad3f6f3b5a223618aa47456675b5cdf5626e584
Vedang <vedang.wappnet@gmail.com>
2025-02-27 14:21:31 +0530

    rabbit-mq url is added

--- config/rabbitmqConfig.js
+++ config/rabbitmqConfig.js
@@ -1,13 +1,15 @@
 const amqp = require('amqplib');
+require('dotenv').config();
 
 const connectRabbitMQ = async () => {
-    try{
-        const connection = await amqp.connect(URL);
+    try {
+        const connection = await amqp.connect(process.env.RABBITMQ_URL);
         const channel = await connection.createChannel();
-        console.log('Connected to RabbitMQ');
-        return { connection, channel };
-    }catch(error){
-        console.error("RabbitMQ Connection Error: ", error.message);
+        await channel.assertQueue('notifications', { durable: true });
+        console.log('✅ Connected to RabbitMQ');
+        return channel;
+    } catch (error) {
+        console.error('❌ RabbitMQ Connection Error:', error.message);
     }
 };
 
